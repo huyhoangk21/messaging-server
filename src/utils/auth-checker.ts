a3848725730dfc0ee2829b319dad29a545d29549
Hoang Le <kuhpro@gmail.com>
2022-02-23 21:55:26 -0600

    added logger for auth checker function

--- src/utils/auth-checker.ts
+++ src/utils/auth-checker.ts
@@ -9,6 +9,7 @@ export const authChecker: AuthChecker<IContext> = async (
   try {
     const { user } = context;
     if (roles.length === 0) return user !== undefined;
+    console.log('im here');
     if (roles.includes(user.role)) return true;
     return false;
   } catch (error) {
